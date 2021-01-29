0bfd7c3d189b5ab807defa533958d72764887971
Mg Kaung <mymemory692018@gmail.com>
2021-01-29 13:16:00 +0630

    ---

--- app/src/main/java/mm/etopup/com/viewholder/AmountViewHolder.java
+++ app/src/main/java/mm/etopup/com/viewholder/AmountViewHolder.java
@@ -1,11 +1,12 @@
 package mm.etopup.com.viewholder;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import de.hdodenhof.circleimageview.CircleImageView;
 import mm.etopup.com.R;
 import mm.etopup.com.base.views.holders.BaseViewHolder;
 
@@ -13,6 +14,12 @@ public class AmountViewHolder <T> extends BaseViewHolder<T> {
 
     @BindView(R.id.amount_text)
     TextView amount_text;
+
+    @BindView(R.id.checkimage)
+    ImageView checkimage;
+
+    @BindView(R.id.amount_layout)
+    LinearLayout amount_layout;
 
 
     public AmountViewHolder(View itemView) {
