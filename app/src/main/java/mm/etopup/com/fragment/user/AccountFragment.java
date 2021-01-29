adf5fbba31f05bfcb156e7ac910712ba18f3e63b
Mg Kaung <mymemory692018@gmail.com>
2021-01-29 13:58:53 +0630

    logout for user

--- app/src/main/java/mm/etopup/com/fragment/user/AccountFragment.java
+++ app/src/main/java/mm/etopup/com/fragment/user/AccountFragment.java
@@ -6,11 +6,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import mm.etopup.com.R;
+import mm.etopup.com.activities.SplashActivity;
+import mm.etopup.com.session.SessionManager;
 
 public class AccountFragment extends Fragment {
+
+
+    @BindView(R.id.logoutButton)
+    AppCompatButton logoutButton;
 
     public static AccountFragment newInstance() {
         AccountFragment fragment = new AccountFragment();
@@ -21,7 +31,20 @@ public class AccountFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_account, container, false);
+        ButterKnife.bind(this, root);
+        setUpListener();
         return root;
     }
 
+    private void setUpListener()
+    {
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SessionManager.getObjectInstance(getActivity()).setLoggedIn(false);
+                getActivity().finish();
+                SplashActivity.open(getActivity());
+            }
+        });
+    }
 }
